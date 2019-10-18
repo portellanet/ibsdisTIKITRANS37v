6e1a8f1ad90c8d1ba50261f721ad53598ad82ab4
portellanet <56727249+portellanet@users.noreply.github.com>
2019-10-18 15:14:48 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ function locationHashChanged() {
 if(window.location.hash) {
   document.getElementById("searchBg").style.display = "none";
   document.getElementById("homeHero").style.display = "none";
+  document.getElementById("wdps").style.display = "none";
+  document.getElementById("advantages").style.display = "none";  
 }
 }
 window.onload = locationHashChanged;
